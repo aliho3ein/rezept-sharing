9642a7b6532fecc750a48f68b833b491b21486bb
JanFolz <jan.folz@dci.education>
2023-08-08 20:55:16 +0200

    build user-profile-page/recipe-form

---
+++ src/components/userProfile/AddRecipeForm.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const AddRecipeForm: FC = () => {
+  return (
+    <fieldset>
+      <form>
+        <input type="text" />
+      </form>
+    </fieldset>
+  );
+};
+
+export default AddRecipeForm;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { FC } from "react";
 import StartPage from "../components/startPage/Index";
+import UserProfile from "./UserProfile";
 
 const App: FC = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<StartPage />} />
+        <Route path="/user-profile" element={<UserProfile />} />
       </Routes>
     </>
   );
