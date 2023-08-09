f5b8e058c0a23644ff6454ddbf87f71b9e29709e
JanFolz <jan.folz@dci.education>
2023-08-09 20:50:31 +0200

    build recipe form

--- src/components/userProfile/AddRecipeForm.tsx
+++ src/components/userProfile/AddRecipeForm.tsx
@@ -4,7 +4,16 @@ const AddRecipeForm: FC = () => {
   return (
     <fieldset>
       <form>
-        <input type="text" />
+        <input type="text" placeholder="Ingredient" />
+        <input type="number" id="ingredient-amount" min="1" />
+        <select id="ingredient-unit">
+          <option value="g">g</option>
+          <option value="ml">ml</option>
+          <option value="Tasse">Tasse</option>
+          <option value="TL">TL</option>
+          <option value="EL">EL</option>
+        </select>
+        <textarea id="cooking-process" placeholder="Zubereitung..."></textarea>
       </form>
     </fieldset>
   );
