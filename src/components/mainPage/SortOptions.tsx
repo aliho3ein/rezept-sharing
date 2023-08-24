3f6a39d7dfd8ee35c6374d5e1f8350d9c4d49aa4
Charbel222 <Charbel.elherrera@dci.education>
2023-08-24 19:12:35 +0200

    fixed the filerOptions component

--- src/components/mainPage/SortOptions.tsx
+++ src/components/mainPage/SortOptions.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
 import style from "../../styles/mainPage/main.module.scss";
 
-interface sortType {
+/* interface sortType {
   // eslint-disable-next-line @typescript-eslint/ban-types
   changeSort: Function;
+} */
+interface sortType {
+  changeSort: (sortAction: string) => void;
 }
 
 const SortOptions: FC<sortType> = ({ changeSort }) => {
@@ -12,15 +15,28 @@ const SortOptions: FC<sortType> = ({ changeSort }) => {
     { label: "Zeit", action: "time" },
     { label: "Meist Gesehene", action: "view" },
   ];
+  const handleSortChange = (sortAction: string) => {
+    changeSort(sortAction);
+    console.log("changeSort", changeSort(sortAction));
+  };
 
   return (
-    <div className={style.sortOptions}>
-      <label>Sort Options:</label>
+    <div className={style.sortOptionsContainer}>
+      <label className={style.labelSortTile}>Sort Options:</label>
       <ul className={style.sortList}>
         {sortOptions.map((sortOption, index) => (
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-          <li key={index} onClick={() => changeSort(sortOption.action)}>
-            {sortOption.label}
+          <li key={index}>
+            <input
+              type="radio"
+              id={sortOption.action}
+              name="sortOption"
+              value={sortOption.action}
+              onChange={() => handleSortChange(sortOption.action)}
+              className={style.sortInput}
+            />
+            <label htmlFor={sortOption.action} className={style.sortLabel}>
+              {sortOption.label}
+            </label>
           </li>
         ))}
       </ul>
