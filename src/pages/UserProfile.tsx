047b4d582d398266d011f7d7ebf6803723f50e6b
JanFolz <jan.folz@dci.education>
2023-08-29 08:00:40 +0200

    fix form / build user profile page

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,22 +1,25 @@
-import { FC } from "react";
-// import { Link } from "react-router-dom";
-// import style from "../styles/userProfile/userProfile.module.scss";
+import { FC, useState } from "react";
+import { Link } from "react-router-dom";
+import style from "../styles/userProfile/userProfile.module.scss";
 import AddRecipeForm from "../components/userProfile/AddRecipeForm";
 
 const UserProfile: FC = () => {
+  const [popup, setPopup] = useState<boolean>(false);
+
   return (
     <>
-      {/* <button>
+      {popup && <AddRecipeForm closePopup={setPopup} />}
+      <button>
         <Link to="/">
           <i className={`fa-solid fa-reply ${style.returnArrow}`}></i>
-          Zurück
+          Zurück zum Feed
         </Link>
       </button>
+      <header></header>
       <span>Rezept hinzufügen</span>
-      <button>
+      <button onClick={() => setPopup(true)}>
         <i className={`fa-solid fa-plus ${style.addRecipe}`}></i>
-      </button> */}
-      <AddRecipeForm />
+      </button>
     </>
   );
 };
